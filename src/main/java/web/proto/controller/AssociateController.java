94a2df06578fef74f42443cc26d1c61f9c15c7af
Rose Gates <pedroluiz2109@gmail.com>
2023-06-11 02:40:39 -0300

    done: associate crud

--- src/main/java/web/proto/controller/AssociateController.java
+++ src/main/java/web/proto/controller/AssociateController.java
@@ -1,5 +1,7 @@
 package web.proto.controller;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,14 +12,18 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.data.web.SortDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import groovy.util.logging.Slf4j;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import web.proto.model.Associate;
+import web.proto.model.enums.Status;
 import web.proto.model.filter.AssociateFilter;
 import web.proto.pagination.PageWrapper;
 import web.proto.repository.AssociateRepository;
@@ -26,13 +32,13 @@ import web.proto.service.AssociateService;
 @Controller
 @RequestMapping("/associates")
 @Slf4j
-public class AssociateController { 
-    
+public class AssociateController {
+
     private static final Logger logger = LoggerFactory.getLogger(AssociateController.class);
 
     @Autowired
     private AssociateRepository repository;
-    
+
     @Autowired
     private AssociateService service;
 
@@ -43,9 +49,7 @@ public class AssociateController {
 
     @GetMapping("/search")
     public String search(AssociateFilter filtro, Model model,
-            @PageableDefault(size = 10)
-            @SortDefault(sort = "id", direction = Sort.Direction.ASC) 
-            Pageable pageable,
+            @PageableDefault(size = 10) @SortDefault(sort = "id", direction = Sort.Direction.ASC) Pageable pageable,
             HttpServletRequest request) {
         Page<Associate> pagina = repository.filtrar(filtro, pageable);
         PageWrapper<Associate> paginaWrapper = new PageWrapper<>(pagina, request);
@@ -53,9 +57,9 @@ public class AssociateController {
         model.addAttribute("pagina", paginaWrapper);
         return "associates/associates";
     }
-    
+
     @GetMapping("/save")
-	public String showRegisterForm(Model model) {
+    public String showRegisterForm(Model model) {
         model.addAttribute("associate", new Associate());
         model.addAttribute("title", "Register Associate");
         model.addAttribute("url", "/associates/save");
@@ -67,6 +71,45 @@ public class AssociateController {
     public String createAssociate(@ModelAttribute("associate") Associate associate) {
         service.save(associate);
         return "redirect:/associates";
+    }
+
+    @PostMapping("/openupdate")
+    public String openUpdate(Long id, Model model) {
+            Optional<Associate> opt = repository.findById(id);
+            Associate associate = opt.get();
+            model.addAttribute("associate", associate);
+            model.addAttribute("title", "Update Associate");
+            model.addAttribute("url", "/associates/update");
+            model.addAttribute("btn", "Update");
+            return "associates/save";
+    }
+
+    @PostMapping("/update")
+    public String update(Associate associate, Model model) {
+        service.save(associate);
+        return "mostrarmensagem";
+    }
+
+    @PostMapping("/abrirremover")
+    public String openDelete(Associate associate) {
+        return "associates/delete";
+    }
+
+    @PostMapping("/delete")
+    public String delete(Long id, Model model) {
+        Optional<Associate> optLote = repository.findById(id);
+        if (optLote.isPresent()) {
+            Associate associate = optLote.get();
+            associate.setStatus(Status.INACTIVE);
+            service.save(associate);
+            model.addAttribute("option", "associate");
+            model.addAttribute("message", "Excluiu");
+            return "mostrarmensagem";
+        } else {
+            model.addAttribute("message", "There is no person with code: " + id);
+            model.addAttribute("option", "associate");
+            return "mostrarmensagem";
+        }
     }
 
 }
--- src/main/java/web/proto/repository/AssociateRepository.java
+++ src/main/java/web/proto/repository/AssociateRepository.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import web.proto.model.Associate;
+import web.proto.model.enums.Status;
 import web.proto.repository.queries.associate.AssociateQueries;
 
 public interface AssociateRepository extends JpaRepository<Associate, Long>,  AssociateQueries {
+
+    List<Associate> findByStatus(Status status);
 
 }
