3b802ae7412187bde6c5c9a4a7c88468869235b5
Rose Gates <pedroluiz2109@gmail.com>
2023-06-14 18:23:56 -0300

    fix: repository

--- src/main/java/web/proto/repository/PhaseRepository.java
+++ src/main/java/web/proto/repository/PhaseRepository.java
@@ -1,11 +1,9 @@
 package web.proto.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import web.proto.model.Phase;
 
-@Repository
 public interface PhaseRepository extends JpaRepository<Phase, Long>{
     
 }
