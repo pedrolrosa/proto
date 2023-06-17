4995422dd864a871f71c4155eb3a96acf0a65ff5
Rose Gates <pedroluiz2109@gmail.com>
2023-06-16 22:18:39 -0300

    add: login

---
+++ src/main/java/web/proto/config/SecurityConfig.java
@@ -0,0 +1,67 @@
+package web.proto.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests(configurer -> configurer
+				.requestMatchers("/css/**", "/js/**", "/images/**", "/", "/index.html").permitAll()
+				.requestMatchers("/api/**").hasAuthority("ADM")
+				.anyRequest().permitAll()
+			)
+			.formLogin(form -> form
+				.loginPage("/login")
+				//.failureUrl("/login-error")
+				.defaultSuccessUrl("/")
+				.permitAll()
+			)
+            .logout(logout -> logout
+				.permitAll()
+
+				.logoutSuccessUrl("/")
+			);
+		return http.build();
+	}
+
+	@Bean
+ 	public UserDetailsService userDetailsService(DataSource dataSource) {    	
+		JdbcUserDetailsManager manager = new JdbcUserDetailsManager(dataSource);
+		manager.setUsersByUsernameQuery("select login, password, active "
+			  						+ "from associates "
+			  						+ "where login = ?");
+		manager.setAuthoritiesByUsernameQuery(
+			  "SELECT login , role FROM associates WHERE login = ?");
+		return manager;
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		
+		String idEnconder = "argon2";
+		Map<String, PasswordEncoder> encoders = new HashMap<>();
+		encoders.put("argon2", Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8());
+		encoders.put("noop", NoOpPasswordEncoder.getInstance());
+
+		PasswordEncoder passwordEncoder = new DelegatingPasswordEncoder(idEnconder, encoders);
+		return passwordEncoder;
+	}
+
+}
---
+++ src/main/java/web/proto/controller/IndexController.java
@@ -0,0 +1,19 @@
+package web.proto.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    
+    @GetMapping(value = {"/", "/index.html"} )
+	public String index() {
+		return "index";
+	}
+
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+
+}
